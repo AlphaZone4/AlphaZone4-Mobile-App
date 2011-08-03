269f297077937e53dfba97c3f4fb310e991945f4
unknown <Jamie@.(none)>
2011-08-03 12:42:03 +0100

    Added readme and removed some debug test code

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,5 @@
 // global useful functions
 
-	/* setup event listener to fire JS when device is ready */
-	
-	
-    var onDeviceReady = function(){
-    	alert("FDSAFDS");
-        if (connectionCheck()){
-        	msg("Online!");
-        }else{
-        	msg("Offline :(");
-        }
-    }
-
-    
-    document.addEventListener("deviceready", onDeviceReady, false);
 /*
  * Test the current device online connection
  * returns true if internet access is available, false otherwise
